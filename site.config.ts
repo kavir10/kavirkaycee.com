4601d043c356990c11877515c8ded4ac194b9099
kavir10 <kavir.kaycee1@gmail.com>
2024-02-26 18:09:21 +0530

    first changes

--- site.config.ts
+++ site.config.ts
@@ -2,32 +2,31 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7167d2cf133a4875b7e23caf545ee402',
+  rootNotionPageId: '02d7e8dabdc048f682724fba25bfc2c7',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'wr8 Demo',
-  domain: 'demo.wr8.in',
-  author: 'Mighil',
-  seotitle: 'Demo Site Created with wr8',
+  name: 'Kavir Kaycee',
+  domain: 'x',
+  author: 'Kavir Kaycee',
+  seotitle: 'Kavir Kaycee | Product Management',
 
   // open graph metadata (optional)
-  description: 'This is a demo website created with wr8.',
+  description: 'Hi! I am Kavir, a Product Manager, Builder, Startup Advisor, and Writer. This is my small corner of the internet.',
 
   // social usernames (optional)
-  twitter: 'jotzilla',
-  github: 'verfasor',
-  linkedin: 'mighil',
-  // newsletter: '#', // optional newsletter URL
+  twitter: 'kavkaycee',
+  linkedin: 'kavirkaycee',
+  newsletter: 'https://thediscourse.co', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: 'https://wr8.in/page-icon.png',
-  defaultPageCover: 'https://wr8.in/page-cover.jpg',
+  defaultPageIcon: 'https://www.notion.so/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Fcb5cda98-0a30-431a-bc0a-006e5d8770c3%2F0031064a-5312-41cf-bef6-551f9de3ff92%2FKavir_circle_notion_dp_profile.webp?table=block&id=02d7e8da-bdc0-48f6-8272-4fba25bfc2c7&spaceId=cb5cda98-0a30-431a-bc0a-006e5d8770c3&width=250&userId=74bd6286-8f72-4167-b6c4-af3c877a1fba&cache=v2',
+  defaultPageCover: 'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Faea24c45-d89d-44bc-9b0a-7f7864fcd23e%2FCoverV1.1.png?table=block&id=0972cf4b-f5f4-4698-aa72-78b8773a4fbd&cache=v2',
   defaultPageCoverPosition: 0.5,
 
   // whether or not to enable support for LQIP preview images (optional)
@@ -53,16 +52,12 @@ export default siteConfig({
   navigationStyle: 'custom',
   navigationLinks: [   
     {
-      title: 'Blog',
-      pageId: '588031c1e65d4dfa9d58e2492c9c1b7e'
+      title: 'Posts',
+      pageId: '9cecccc56cca48839ceff3dc442e65f2'
     },    
     {
-      title: 'About',
-      pageId: 'cf2ae3c4839e4e09a1b4a33cc600c409'
-    },
-    {
-      title: 'Contact',
-      pageId: '5f7e562fef814299b10c68c52e920c9a'
+      title: 'GPTs',
+      pageId: 'b890bb9f0f0b42e4ba2bcc927d91f26d'
     }
   ]
 })
