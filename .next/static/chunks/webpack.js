4644281ba5cb369954c6bc461bc8284a76a8faae
kavir10 <kavir.kaycee1@gmail.com>
2024-04-14 23:19:12 +0530

    font aliazing

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "46eb03442a9d3d55"; }
+/******/ 		__webpack_require__.h = function() { return "5fcbfe1e61e8f365"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
